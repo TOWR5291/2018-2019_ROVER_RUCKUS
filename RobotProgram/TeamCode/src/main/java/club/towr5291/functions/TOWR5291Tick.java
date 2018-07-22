3784f17d8f42216a6336f638dc2c19d3b561246e
The Oxford Robotics Wildcats 5291 Robotics Team <towr5291@gmail.com>
2018-07-22 08:44:07 -0400

    Updated Tick with some extra functionality and comments

--- RobotProgram/TeamCode/src/main/java/club/towr5291/functions/TOWR5291Tick.java
+++ RobotProgram/TeamCode/src/main/java/club/towr5291/functions/TOWR5291Tick.java
@@ -3,88 +3,168 @@ package club.towr5291.functions;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * Created by Wyatt on 7/19/2018.
- */
 
+/**
+ * Created by Wyatt Ashley TOWR5291 on 7/19/2018.
+ * This class creates an object that increments up to a set point then rolls back over to the minimum setpoint
+ * as well as can decrement down to a minimum set point then roll over to a maximum setpoint
+ *
+ * Modification history
+ * Edited by:
+ * Wyatt Ashley 07/20/2018 ->  Initial creation
+ * Ian Haden 07/22/2018 -> Added comments and removed soem unneed steps, renamed some variables so they represent their respective functions
+ */
 public class TOWR5291Tick {
 
-    public double TOWR5291ToggleTickNumber = 0.1;
+    private double TOWR5291ToggleTickNumber = 0.1;
     private double increment = 0.1;
     private double TOWR5291ToggleTickMax = 1;
     private double TOWR5291ToggleTickMin = 0;
-    private boolean lastState;
-    private boolean lastState1;
-    private int debounceTime = 1000;
-    private ElapsedTime runtime;
-    private ElapsedTime runtime2;
+    private boolean lastStateIncrement = false;
+    private boolean lastStateDecrement = false;
+    private int debounceTime = 100;
+    private ElapsedTime debounceTimerIncrement;
+    private ElapsedTime debounceTimerDecrement;
+    boolean rollover = true;
 
+    /**
+     * Constructor, initialises all variables
+     *
+     */
     public TOWR5291Tick(){
-         runtime = new ElapsedTime();
-         runtime2 = new ElapsedTime();
+        this.debounceTimerIncrement = new ElapsedTime();
+        this.debounceTimerDecrement = new ElapsedTime();
     }
 
+    /**
+     * Constructor, initialises all variables
+     * over rides the initial states in the event the initial states could be TRUE rather than th default FALSE
+     */
+    public TOWR5291Tick(boolean initialStateIncrement, boolean initialStateDecrement){
+        this.debounceTimerIncrement = new ElapsedTime();
+        this.debounceTimerDecrement = new ElapsedTime();
+        this.lastStateIncrement = initialStateIncrement;
+        this.lastStateDecrement = initialStateDecrement;
+    }
+
+    /**
+     * Increment the value of the object and check if it has reached its maximum value
+     * If the maximum value has been reached then roll over to the minimum
+     *
+     * @param input Takes a true or false value to increment the value.
+     */
     public void incrementTick(boolean input){
-        if (this.lastState != input && runtime.milliseconds() > debounceTime) {
-            this.lastState = input;
-            runtime.reset();
+        if (this.lastStateIncrement != input && debounceTimerIncrement.milliseconds() > this.debounceTime) {
+            this.lastStateIncrement = input;
+            debounceTimerIncrement.reset();
             if (input == true) {
-                this.TOWR5291ToggleTickNumber += increment;
+                this.TOWR5291ToggleTickNumber += this.increment;
                 if (this.TOWR5291ToggleTickNumber > this.TOWR5291ToggleTickMax) {
-                    this.TOWR5291ToggleTickNumber = TOWR5291ToggleTickMin;
-                }
-                if (this.TOWR5291ToggleTickNumber <= TOWR5291ToggleTickMin) {
-                    this.TOWR5291ToggleTickNumber = 0.0;
+                    if (rollover) {
+                        this.TOWR5291ToggleTickNumber = TOWR5291ToggleTickMin;
+                    } else {
+                        //no rollover so return it to to max
+                        this.TOWR5291ToggleTickNumber = TOWR5291ToggleTickMax;
+                    }
+
                 }
             }
         }
     }
 
+    /**
+     * Decrement the value of the object and check if it has reached the minimum value
+     * if the minimum has been reached roll back over to the maximum
+     *
+     * @param input Takes a true or false value to decrement that value
+     */
     public void decrementTick(boolean input){
-        if (this.lastState1 != input || runtime2.milliseconds() > debounceTime) {
-            this.lastState1 = input;
-            runtime2.reset();
+        if (this.lastStateDecrement != input && debounceTimerDecrement.milliseconds() > this.debounceTime) {
+            this.lastStateDecrement = input;
+            debounceTimerDecrement.reset();
             if (input == true) {
-                this.TOWR5291ToggleTickNumber -= increment;
-                if (this.TOWR5291ToggleTickNumber > this.TOWR5291ToggleTickMax) {
-                    this.TOWR5291ToggleTickNumber = TOWR5291ToggleTickMin;
-                }
+                this.TOWR5291ToggleTickNumber -= this.increment;
                 if (this.TOWR5291ToggleTickNumber <= TOWR5291ToggleTickMin) {
-                    this.TOWR5291ToggleTickNumber = 1.0;
+                    if (rollover) {
+                        this.TOWR5291ToggleTickNumber = TOWR5291ToggleTickMax;
+                    } else {
+                        this.TOWR5291ToggleTickNumber = TOWR5291ToggleTickMin;
+                    }
                 }
             }
         }
     }
 
+
+    /**
+     * Set the value of the increment and decrement
+     *
+     * @param onoff Set whether the object should rollover or stay at Max or Min
+     */
+    public void setRollOver(boolean onoff){
+        this.rollover = onoff;
+    }
+
+
+    /**
+     * Set the value of the increment and decrement
+     *
+     * @param incrementAmount Value each time a change occurs
+     */
     public void setIncrement(double incrementAmount){
         this.increment = incrementAmount;
     }
 
-    public void setMaxTick(double maxTick){
-        this.TOWR5291ToggleTickMax = maxTick;
+    /**
+     * Set the maximum value of the object
+     *
+     * @param maximum Maximum Value before rolling over
+     */
+    public void setMaxTick(double maximum){
+        this.TOWR5291ToggleTickMax = maximum;
     }
 
-    public void setTOWR5291ToggleTickMin(double Minimum){
-        this.TOWR5291ToggleTickMin = Minimum;
+    /**
+     * Set the minimum value of the object
+     *
+     * @param minimum Minimum Value before rolling over
+     */
+    public void setMinTick(double minimum){
+        this.TOWR5291ToggleTickMin = minimum;
     }
 
+    /**
+     * Get the current value
+     */
     public double getNumberTick(){
         return roundDown(this.TOWR5291ToggleTickNumber);
+    }
+
+    /**
+     * Get the minimum setting value
+     */
+    public double getTickMin() {
+        return this.TOWR5291ToggleTickMin;
+    }
+
+    /**
+     * Get the maximum setting value
+     */
+    public double getTickMax() {
+        return this.TOWR5291ToggleTickMax;
+    }
+
+    /**
+     * Set or Over ride the current value
+     */
+    public void setTickNumberValue(double value){
+        if ((value <= TOWR5291ToggleTickMax) && (value >= TOWR5291ToggleTickMin)){
+            this.TOWR5291ToggleTickNumber = value;
+        }
     }
 
     private static double roundDown(double d) {
         return (long) (d * 1e1) / 1e1;
     }
 
-    public double getTOWR5291ToggleTickMin() {
-        return this.TOWR5291ToggleTickMin;
-    }
-
-    public double getTOWR5291ToggleTickMax() {
-        return this.TOWR5291ToggleTickMax;
-    }
-
-    public void setTOWR5291ToggleTickNumberValue(double Value){
-        this.TOWR5291ToggleTickNumber = Value;
-    }
 }
